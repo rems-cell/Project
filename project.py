43d1a0c84c5448f65c734535c73839bdc444894b
rems-cell <qrnrabas@tip.edu.ph>
2024-11-30 11:53:44 +0800

    Update project.py

--- project.py
+++ project.py
@@ -59,5 +59,5 @@ else:
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
     class_names = ['shine', 'rainy', 'sunrise', 'snowy', 'cloudy']
-    string = "OUTPUT : " + class_names[np.argmax(prediction)]
+    string = "Weather : " + class_names[np.argmax(prediction)]
     st.success(string)
